43c5a0441ea45facaa8e2a750e9a7d895049bb59
manjunath ramareddy <manjunath.ramareddy@gmail.com>
2020-02-14 16:33:45 +0530

    final commit

--- src/test/java/startbootstrap/project/RegisterStartbootstrap.java
+++ src/test/java/startbootstrap/project/RegisterStartbootstrap.java
@@ -18,7 +18,7 @@ public class RegisterStartbootstrap {
 	}
 	
 	@Test
-	public void fillRegistration() {
+	public void Registration() {
 		driver.findElement(By.xpath("//input[@id='exampleFirstName']")).sendKeys("Manjunath");
 		driver.findElement(By.xpath("//input[@id='exampleLastName']")).sendKeys("Mindtree");
 		driver.findElement(By.xpath("//input[@id='exampleInputEmail']")).sendKeys("Manjunathreddy@gmail.com");
@@ -27,11 +27,17 @@ public class RegisterStartbootstrap {
 		driver.findElement(By.xpath("//a[contains(text(),'Register Account')]")).click();
 		System.out.println("Registration is successful");
 	}
+	
+	@Test
+	public void Login() {
+		driver.get("http://localhost:9090/startbootstrap/login.html");
+		driver.findElement(By.xpath("//input[@id='exampleInputEmail']")).sendKeys("Manjunathreddy@gmail.com");
+		driver.findElement(By.xpath("//input[@id='exampleInputPassword']")).sendKeys("Manju@123");
+		driver.findElement(By.xpath("//a[contains(text(),'Login')]")).click();
+		System.out.println("Login is successful");
+	}
 	@AfterMethod
 	public void closeBrowser() {
 		driver.close();
 	}
-	
-	
-
 }
